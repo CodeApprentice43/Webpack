fbe38fdc46af7286ac432050b18855281bc2b030
CodeApprentice43 <121590969+CodeApprentice43@users.noreply.github.com>
2023-05-01 17:37:07 -0400

    configured and experimented with webpack

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'main.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  devServer: {
+    static: path.join(__dirname, 'build'),
+    compress: true,
+    port: 3000,
+  },
+  devtool: 'source-map',
+
+};
